bc95dbd47ccca87b9ad4e53953e7153010305e53
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-05-12 08:46:44 +0200

    fix

--- reworn-app/src/components/popular/Popular.jsx
+++ reworn-app/src/components/popular/Popular.jsx
@@ -1,20 +1,27 @@
-import React from "react";
 import SectionTitle from "../SectionTitle";
 import Card from "../Card";
-import girl1 from "../../assets/clothes/girl1.png";
-import girl2 from "../../assets/clothes/girl2.png";
-import girl3 from "../../assets/clothes/girl3.png";
-import girl4 from "../../assets/clothes/girl4.png";
+import redTshirt from "../../assets/clothes/redTshirt-Photoroom.jpg";
+import whiteBlueShirt from "../../assets/clothes/whiteBlueShirt-Photoroom.jpg";
+import jacket from "../../assets/clothes/jacket-Photoroom.jpg";
+import mantle from "../../assets/clothes/mantle-Photoroom.jpg";
 
 function Popular() {
   return (
     <section id="latest" className="px-1 md:px-24 h-auto">
-      <SectionTitle title="Popular this week" />
+      <SectionTitle title="Rrobat e javës" />
       <div className="grid grid-cols-4 gap-10 h-auto place-items-center">
-        <Card image={girl1} alt="Girl 1" descr="Lorem ipsum dolor sit amet." />
-        <Card image={girl2} alt="Girl 2" descr="Lorem ipsum dolor sit amet." />
-        <Card image={girl3} alt="Girl 3" descr="Lorem ipsum dolor sit amet." />
-        <Card image={girl4} alt="Girl 4" descr="Lorem ipsum dolor sit amet." />
+        <Card
+          image={redTshirt}
+          alt="Girl 1"
+          descr="Lorem ipsum dolor sit amet."
+        />
+        <Card image={jacket} alt="Girl 2" descr="Lorem ipsum dolor sit amet." />
+        <Card
+          image={whiteBlueShirt}
+          alt="Girl 3"
+          descr="Lorem ipsum dolor sit amet."
+        />
+        <Card image={mantle} alt="Girl 4" descr="Lorem ipsum dolor sit amet." />
       </div>
     </section>
   );
